5af7f964ce10c222da9c40f0a8caf29e72f32010
rabdiego <diegorabelodesa@gmail.com>
2024-06-07 17:35:30 -0300

    Classe da BackingTrack criada, ainda n integrada com SQLite

---
+++ app/src/main/java/com/es1/backingquack/BackingTrack.kt
@@ -0,0 +1,30 @@
+import java.io.File
+import org.jfugue.midi.MidiFileManager
+import org.jfugue.pattern.Pattern
+import org.jfugue.theory.ChordProgression
+
+class BackingTrack(val chordProgressionList: List <String>, val root: String, val bpm: Int) {
+    private fun convertChordProgressionString(chordProgressionList: List <String>): String {
+        var finalString: String = ""
+
+        for (chordProgression in chordProgressionList) {
+            val chords: List <String> = chordProgression.split(" ")
+            for (chord in chords) {
+                for (i in 1..4) {
+                    finalString += "$chord "
+                }
+            }
+        }
+
+        return finalString
+    }
+
+    fun build() {
+        val chordProgression = this.convertChordProgressionString(this.chordProgressionList)
+
+        val cp = ChordProgression(chordProgression).setKey(root)
+        val pattern = Pattern(cp).setTempo(bpm)
+        val file = File("/home/diego/Documents/kotlinstudies/src/pattern.midi")
+        MidiFileManager.savePatternToMidi(pattern, file)
+    }
+}
