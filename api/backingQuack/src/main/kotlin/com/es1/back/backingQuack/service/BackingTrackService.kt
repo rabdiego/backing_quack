3326ad022164b72dbff523fb90b23dfde63f8063
rabdiego <diegorabelodesa@gmail.com>
2024-06-23 21:25:53 -0300

    Added Rhythm

--- api/backingQuack/src/main/kotlin/com/es1/back/backingQuack/service/BackingTrackService.kt
+++ api/backingQuack/src/main/kotlin/com/es1/back/backingQuack/service/BackingTrackService.kt
@@ -4,6 +4,7 @@ import com.es1.back.backingQuack.model.BackingTrackRequest
 import org.jfugue.midi.MidiFileManager
 import org.jfugue.pattern.Pattern
 import org.jfugue.theory.ChordProgression
+import org.jfugue.rhythm.Rhythm;
 import org.springframework.stereotype.Service
 import java.io.File
 import java.nio.file.Files
@@ -17,7 +18,10 @@ class BackingTrackService {
     fun generateMidi(request: BackingTrackRequest): Pair<File, UUID> {
         val chordProgression = convertChordProgressionString(request.chordProgressionList)
         val cp = ChordProgression(chordProgression).setKey(request.root)
-        val pattern = Pattern(cp).setTempo(request.bpm)
+        val rhythm= Rhythm()
+        rhythm.addLayer("O...").addLayer("````")
+        val repeats = getNumberOfRepeats(request.chordProgressionList)
+        val pattern = Pattern(cp, rhythm.pattern.repeat(repeats)).setTempo(request.bpm)
 
         val uuid = UUID.randomUUID()
 
@@ -45,5 +49,10 @@ class BackingTrackService {
             }
         }
         return finalString.toString()
+    }
+
+    private fun getNumberOfRepeats(chordProgression: String): Int {
+        val chords: List <String> = chordProgression.split(" ");
+        return 2 * chords.size;
     }
 }
